5a4c021c24b5f24bac91bb2d7b76fa91a93b3119
JF1HNL <kimossan88@gmail.com>
2023-10-24 22:04:10 +0900

    update : js tweet text

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let now = new Date()
 function tweet() {
   const content = {
     url: window.location.href,
-    text: `【退勤】${now.getHours()}時${now.getMinutes()}分 @open_kim_`,
+    text: `【退勤】${now.getHours()}時${now.getMinutes()}分 @open_kim_ #退勤ボタン`,
     tag: ["退勤ボタン"]
   };
   for (let key in content) {
@@ -11,7 +11,7 @@ function tweet() {
   }
   //window.location.href = "https://twitter.com/intent/tweet?url=" + content.url + "&text=" + content.text + "&hashtags=" + content.tag
   //window.open("https://twitter.com/intent/tweet?url=" + content.url + "&text=" + content.text + "&hashtags=" + content.tag)
-  window.open("twitter://post?message=" + content.text + content.url)
+  window.location.href = "twitter://post?message=" + content.text + "\n" + content.url
 }
 
 tweet()
