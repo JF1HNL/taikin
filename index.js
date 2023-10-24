235e25365408c7f288d4a0ab81c886960baa4efd
JF1HNL <kimossan88@gmail.com>
2023-10-24 22:20:13 +0900

    Revert "update : URL"
    This reverts commit 818cb5cebd37c08f2a3b818b15062b1434736496.

--- index.js
+++ index.js
@@ -3,16 +3,15 @@ let now = new Date()
 function tweet() {
   const content = {
     url: window.location.href,
-    text: `【退勤】${now.getHours()}時${now.getMinutes()}分`,
-    tag: ["退勤ボタン"],
-    mention: ["open_kim_"]
+    text: `【退勤】${now.getHours()}時${now.getMinutes()}分 @open_kim_ #退勤ボタン `,
+    tag: ["退勤ボタン"]
   };
   for (let key in content) {
     content[key] = encodeURIComponent(content[key]);
   }
   //window.location.href = "https://twitter.com/intent/tweet?url=" + content.url + "&text=" + content.text + "&hashtags=" + content.tag
   //window.open("https://twitter.com/intent/tweet?url=" + content.url + "&text=" + content.text + "&hashtags=" + content.tag)
-  window.location.href = "twitter://post?message=" + content.text + "&mention=" + content.mention + "&hashtags=" + content.tag + "&url=" + content.url
+  window.location.href = "twitter://post?message=" + content.text + content.url
 }
 
 tweet()
